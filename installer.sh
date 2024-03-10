ed999ae43064a566bec910b4b03346ff5a254c9b
Abhinav Borah <imabhinavborah@gmail.com>
2024-03-11 02:32:32 +0530

    fix github repo link

--- installer.sh
+++ installer.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
 if [ ! -x "$0" ]; then
-    echo "Please make the script executable by running:\nchmod +x $0"
-    exit 1
+	echo "Please make the script executable by running:\nchmod +x $0"
+	exit 1
 fi
 
 install() {
-    # move nvim config directories to backup
-    mv ~/.config/nvim{,.bak}
+	# move nvim config directories to backup
+	mv ~/.config/nvim{,.bak}
 
-    # create backups as a failsafe
-    mv ~/.local/share/nvim{,.bak}
-    mv ~/.local/state/nvim{,.bak}
-    mv ~/.cache/nvim{,.bak}
+	# create backups as a failsafe
+	mv ~/.local/share/nvim{,.bak}
+	mv ~/.local/state/nvim{,.bak}
+	mv ~/.cache/nvim{,.bak}
 
-    # clone my repo to local
-    git clone https://github.com/imabhinavborah/nvim-starter ~/.config/nvim
+	# clone my repo to local
+	git clone https://github.com/abhinavborah/nvim-starter ~/.config/nvim
 }
 
 {
-    install
+	install
 } || {
-    echo "Installation failed, please report the issue."
+	echo "Installation failed, please report the issue."
 }
 
 exit 0
+
