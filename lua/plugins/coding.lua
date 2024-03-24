5d5eff7e378e808ae1d75f1b28780c483d88c6af
Abhinav Borah <imabhinavborah@gmail.com>
2024-03-24 10:55:27 +0530

    feat add codeium

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,12 +1,34 @@
 return {
   -- multicursor
   { "mg979/vim-visual-multi" },
+  -- tidalcycles
+  { "tidalcycles/vim-tidal" },
   -- lazygit
   {
     "kdheepak/lazygit.nvim",
     -- optional for floating window border decoration
     dependencies = {
       "nvim-lua/plenary.nvim",
+    },
+    {
+      "Exafunction/codeium.vim",
+      -- codeium for autocompletion; copilot alternative
+      event = "BufEnter",
+      config = function()
+        -- Change '<C-g>' here to any keycode you like.
+        vim.keymap.set("i", "<C-g>", function()
+          return vim.fn["codeium#Accept"]()
+        end, { expr = true, silent = true })
+        vim.keymap.set("i", "<C-]>", function()
+          return vim.fn["codeium#CycleCompletions"](1)
+        end, { expr = true, silent = true })
+        vim.keymap.set("i", "<C-['>", function()
+          return vim.fn["codeium#CycleCompletions"](-1)
+        end, { expr = true, silent = true })
+        vim.keymap.set("i", "<C-x>", function()
+          return vim.fn["codeium#Clear"]()
+        end, { expr = true, silent = true })
+      end,
     },
   },
 }
