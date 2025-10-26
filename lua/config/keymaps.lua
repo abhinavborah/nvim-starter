97b9dbb56af964b6fb3198f85ce57aaa092d4bdd
Abhinav Borah <imabhinavborah@gmail.com>
2025-10-27 02:26:34 +0530

    add cpp compile macro

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,8 @@
 
 local map = vim.keymap.set
 map("n", "<Tab>", "%", { desc = "Toggle move cursor to start/end of {}" })
+
+vim.keymap.set("n", "<F10>", function()
+  vim.cmd("w") -- Save file
+  vim.cmd("!g++ -fsanitize=address -std=c++17 -Wall -Wextra -Wshadow -DONPC -O2 -o %< % && ./%< < input.txt")
+end, { noremap = true, silent = true, desc = "C++ compile/run with sanitizer" })
